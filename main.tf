62a4b49a161f0993377a10958242eda40fae5bb3
Raghavan R <raghavan.r@capgemini.com>
2018-08-20 15:49:10 +1000

    empty list fix

--- main.tf
+++ main.tf
@@ -2,6 +2,6 @@ resource "aws_route" "route" {
   count                  = "${var.create_vpc && var.nat_gateway_route ? var.count : 0}"
   route_table_id         = "${element(var.route_table_id, count.index)}"
   destination_cidr_block = "${var.destination_cidr_block}"
-  gateway_id = "${var.gateway_route ? element(var.gateway_id, count.index) : "null"}"
-  nat_gateway_id = "${var.nat_gateway_route ? element(var.nat_gateway_id, count.index) : "null"}"
+  gateway_id = "${var.gateway_route ? element(concat(var.gateway_id, list("")), count.index) : "null"}"
+  nat_gateway_id = "${var.nat_gateway_route ? element(concat(var.nat_gateway_id, list("")), count.index) : "null"}"
 }
