a558b0d6dcc423ddf282d781d5f05cf01e97ea00
Raghavan R <raghavan.r@capgemini.com>
2018-08-20 15:13:18 +1000

    Adding route tf components

---
+++ variable.tf
@@ -0,0 +1,22 @@
+variable "route_table_id" {
+  type = "list"
+}
+
+variable "destination_cidr_block" {}
+
+variable "nat_gateway_route" {
+  default = "false"
+}
+
+variable "nat_gateway_id" {
+  type = "list"
+}
+
+variable "create_vpc" {}
+
+variable "count" {}
+
+variable "gateway_route" {
+  type = "list"
+  default = false
+}
